33c410bfc4edebe7871dc88709bc7fba6f1557d2
Rob MacDonald <rob@robmacdonald.com>
2025-01-17 09:35:01 +0000

    Cleaned up formatting in settings.cpp and audio_clip_view.h

--- src/deluge/gui/menu_item/runtime_feature/settings.cpp
+++ src/deluge/gui/menu_item/runtime_feature/settings.cpp
@@ -78,8 +78,7 @@ std::array<MenuItem*, RuntimeFeatureSettingType::MaxElement - kNonTopLevelSettin
     &menuEnableGridViewLoopPads,
     &menuAlternativeTapTempoBehaviour,
     &menuHorizontalMenus,
-	&menuTrimFromStartOfAudioClip
-};
+	&menuTrimFromStartOfAudioClip};
 
 Settings::Settings(l10n::String name, l10n::String title) : menu_item::Submenu(name, title, subMenuEntries) {
 }
